b21a79a003aa3f8dbe7b56d83e662c3e3a419b4a
Kris <kris.kajtna@gmail.com>
2025-04-29 17:22:57 +0200

    Add configuration validation schema and update AppModule imports

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,7 @@ import { AppModule } from './modules/app.module'
 import { ValidationPipe } from '@nestjs/common'
 import cookieParser from 'cookie-parser'
 import express from 'express'
+import Logging from 'library/Logging'
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, {
@@ -21,6 +22,6 @@ async function bootstrap() {
   const PORT = process.env.PORT || 8080
   await app.listen(PORT)
 
-  console.log(`Server started on port = ${await app.getUrl()}`)
+  Logging.log(`Server started on port = ${await app.getUrl()}`)
 }
 bootstrap()
