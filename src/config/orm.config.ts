24094eb6db94ede0b0db8c71daf676ca56684510
Kris <kris.kajtna@gmail.com>
2025-04-29 22:14:56 +0200

    Add User entity and ORM configuration for PostgreSQL connection

---
+++ src/config/orm.config.ts
@@ -0,0 +1,17 @@
+import { ConfigService } from '@nestjs/config'
+import { TypeOrmModuleOptions } from '@nestjs/typeorm'
+import { PostgresConnectionOptions } from 'typeorm/driver/postgres/PostgresConnectionOptions'
+
+type ConfigType = PostgresConnectionOptions & TypeOrmModuleOptions
+type ConnectionOptions = ConfigType
+
+export const ORMConfig = async (configService: ConfigService): Promise<ConnectionOptions> => ({
+  type: 'postgres',
+  host: configService.get('DATABASE_HOST'),
+  port: +configService.get('DATABASE_PORT'),
+  username: configService.get('DATABASE_USERNAME'),
+  password: configService.get('DATABASE_PWD'),
+  database: configService.get('DATABASE_NAME'),
+  entities: ['dist/**/*.entity.ts'],
+  synchronize: true, //only in development
+})
--- src/modules/app.module.ts
+++ src/modules/app.module.ts
@@ -1,6 +1,7 @@
 import { Module } from '@nestjs/common'
 import { ConfigModule } from '@nestjs/config'
 import { configValidationSchema } from 'config/schema.config'
+import { DatabaseModule } from './database/database.module'
 @Module({
   imports: [
     ConfigModule.forRoot({
@@ -8,6 +9,7 @@ import { configValidationSchema } from 'config/schema.config'
       envFilePath: [`.env.${process.env.STAGE}`],
       validationSchema: configValidationSchema,
     }),
+    DatabaseModule,
   ],
   controllers: [],
   providers: [],
