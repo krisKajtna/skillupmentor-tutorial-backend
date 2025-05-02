5b95f0bf64da46207daca083e3a2ba2e168b2a1b
Kris <kris.kajtna@gmail.com>
2025-05-02 19:06:45 +0200

    Add user-related interfaces, controller, service, and module for user management

---
+++ src/interfaces/auth.interface.ts
@@ -0,0 +1,22 @@
+import { User } from 'entities/user.entity'
+import { Request } from 'express'
+
+export interface TokenPayload {
+  name: string
+  sub: string
+  type: JwtType
+}
+
+export interface RequestWithUser extends Request {
+  user: User
+}
+
+export enum JwtType {
+  ACCES_TOKEN = 'ACCES_TOKEN',
+  REFRESH_TOKEN = 'REFRESH_TOKEN',
+}
+
+export enum CookieType {
+  ACCES_TOKEN = 'ACCES_TOKEN',
+  REFRESH_TOKEN = 'REFRESH_TOKEN',
+}
---
+++ src/modules/users/users.controller.ts
@@ -0,0 +1,31 @@
+import {
+  ClassSerializerInterceptor,
+  Controller,
+  Get,
+  HttpCode,
+  HttpStatus,
+  Param,
+  Query,
+  UseInterceptors,
+} from '@nestjs/common'
+import { UsersService } from './users.service'
+import { PaginatedResult } from 'interfaces/paginated-result.interface'
+import { User } from 'entities/user.entity'
+
+@Controller('users')
+@UseInterceptors(ClassSerializerInterceptor)
+export class UsersController {
+  constructor(private readonly userService: UsersService) {}
+
+  @Get()
+  @HttpCode(HttpStatus.OK)
+  async findAll(@Query('page') page: number): Promise<PaginatedResult> {
+    return this.userService.paginate(page)
+  }
+
+  @Get(':id')
+  @HttpCode(HttpStatus.OK)
+  async findOne(@Param('id') id: string): Promise<User> {
+    return this.userService.findById(id)
+  }
+}
