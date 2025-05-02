bb1c71622bb69cb662e6b4040a5cc25ce74d7739
Kris <kris.kajtna@gmail.com>
2025-05-02 17:53:42 +0200

    Add abstract service with CRUD methods and pagination support

---
+++ src/modules/common/abstract.service.ts
@@ -0,0 +1,83 @@
+import { cache } from '@hapi/joi'
+import { Injectable, BadRequestException, InternalServerErrorException } from '@nestjs/common'
+import { PaginatedResult } from 'interfaces/paginated-result.interface'
+import Logging from 'library/Logging'
+import { Repository } from 'typeorm'
+
+@Injectable()
+export abstract class AbstractService {
+  constructor(protected readonly repository: Repository<any>) {}
+
+  async findAll(relations: []): Promise<any[]> {
+    try {
+      return this.repository.find({ relations })
+    } catch (error) {
+      Logging.error(error)
+      throw new InternalServerErrorException('An error occurred while fetching data')
+    }
+  }
+
+  async findBy(condition, relations = []): Promise<any> {
+    try {
+      return this.repository.findOne({
+        where: condition,
+        relations,
+      })
+    } catch (error) {
+      Logging.error(error)
+      throw new InternalServerErrorException(
+        `Something went wrong while searching for an element with condition ${condition}`,
+      )
+    }
+  }
+
+  async findById(id: string, relations = []): Promise<any> {
+    try {
+      const element = await this.repository.findOne({
+        where: { id },
+        relations,
+      })
+      if (!element) {
+        throw new BadRequestException(`Element with id ${id} not found`)
+      }
+      return element
+    } catch (error) {
+      Logging.error(error)
+      throw new InternalServerErrorException(`Something went wrong while searching for an element with id ${id}`)
+    }
+  }
+
+  async remove(id: string): Promise<any> {
+    const element = await this.findById(id)
+    try {
+      return this.repository.remove(element)
+    } catch (error) {
+      Logging.error(error)
+      throw new InternalServerErrorException(`Something went wrong while removing an element with id ${id}`)
+    }
+  }
+
+  async paginate(page = 1, relations = []): Promise<PaginatedResult> {
+    const take = 10
+
+    try {
+      const [data, total] = await this.repository.findAndCount({
+        take,
+        skip: (page - 1) * take,
+        relations,
+      })
+
+      return {
+        data: data,
+        meta: {
+          total,
+          page,
+          last_page: Math.ceil(total / take),
+        },
+      }
+    } catch (error) {
+      Logging.error(error)
+      throw new InternalServerErrorException(`Something went wrong while searching for a paginated element`)
+    }
+  }
+}
