49a3400936d5d0ac189b203dfe6a68c728fd3d1e
Kris <kris.kajtna@gmail.com>
2025-05-02 20:13:32 +0200

    Implement user creation and update logic with validation in UsersService; add bcrypt utility for password hashing and comparison

--- src/modules/users/users.controller.ts
+++ src/modules/users/users.controller.ts
@@ -2,10 +2,12 @@ import {
   Body,
   ClassSerializerInterceptor,
   Controller,
+  Delete,
   Get,
   HttpCode,
   HttpStatus,
   Param,
+  Patch,
   Post,
   Query,
   UseInterceptors,
@@ -13,6 +15,8 @@ import {
 import { UsersService } from './users.service'
 import { PaginatedResult } from 'interfaces/paginated-result.interface'
 import { User } from 'entities/user.entity'
+import { CreateUserDto } from './dto/create-user.dto'
+import { UpdateUserDto } from './dto/update-user.dto'
 
 @Controller('users')
 @UseInterceptors(ClassSerializerInterceptor)
@@ -35,5 +39,17 @@ export class UsersController {
   @HttpCode(HttpStatus.CREATED)
   async create(@Body() createUserDto: CreateUserDto): Promise<User> {
     return this.userService.create(createUserDto)
+  }
+
+  @Patch(':id')
+  @HttpCode(HttpStatus.OK)
+  async update(@Param('id') id: string, @Body() updateUserDto: UpdateUserDto): Promise<User> {
+    return this.userService.update(id, updateUserDto)
+  }
+
+  @Delete(':id')
+  @HttpCode(HttpStatus.OK)
+  async remove(@Param('id') id: string): Promise<User> {
+    return this.userService.remove(id)
   }
 }
--- src/modules/users/users.service.ts
+++ src/modules/users/users.service.ts
@@ -1,5 +1,58 @@
-import { Injectable } from '@nestjs/common'
+import { BadRequestException, Injectable } from '@nestjs/common'
+import { InjectRepository } from '@nestjs/typeorm'
+import { User } from 'entities/user.entity'
 import { AbstractService } from 'modules/common/abstract.service'
+import { Repository } from 'typeorm'
+import { CreateUserDto } from './dto/create-user.dto'
+import Logging from 'library/Logging'
+import { UpdateUserDto } from './dto/update-user.dto'
+import { compareHash, hash } from 'utils/bcrypt'
+import { PostgresErrorCode } from 'helpers/postgresErrorCode.enum'
 
 @Injectable()
-export class UsersService extends AbstractService {}
+export class UsersService extends AbstractService {
+  constructor(@InjectRepository(User) private readonly usersRepository: Repository<User>) {
+    super(usersRepository)
+  }
+
+  async create(createUserDto: CreateUserDto): Promise<User> {
+    const user = await this.findBy({ email: createUserDto.email })
+    if (user) {
+      throw new BadRequestException('user with email already exists')
+    }
+    try {
+      const newUser = this.usersRepository.create({ ...createUserDto })
+      return this.usersRepository.save(newUser)
+    } catch (error) {
+      Logging.error(error)
+      throw new BadRequestException('something went wrong while creating a new user')
+    }
+  }
+
+  async update(id: string, updateUserDto: UpdateUserDto): Promise<User> {
+    const user = (await this.findById(id)) as User
+    const { email, password, confirm_password, role_id, ...data } = updateUserDto
+    if (user.email !== email && email) {
+      user.email = email
+    }
+    if (password && confirm_password) {
+      if (password !== confirm_password) {
+        throw new BadRequestException('passwords do not match')
+      }
+      if (await compareHash(password, user.password)) {
+        throw new BadRequestException('New password canot be the same as your old password')
+      }
+      user.password = await hash(password)
+    }
+    if (role_id) {
+      // user.role = {...user.role, id: role_id}
+    }
+    try {
+    } catch (error) {
+      Logging.error(error)
+      if (error?.code === PostgresErrorCode.UniqueViolation) {
+        throw new BadRequestException('user with that email already exists')
+      }
+    }
+  }
+}
