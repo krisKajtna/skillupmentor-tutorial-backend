02c27cffdeff94b66559b6e4e9b916533e85eb1f
Kris <kris.kajtna@gmail.com>
2025-05-09 01:17:14 +0200

    Add Orders module, controller, and service for order management

--- src/modules/orders/orders.service.ts
+++ src/modules/orders/orders.service.ts
@@ -42,5 +42,29 @@ export class OrdersService extends AbstractService {
         })
       })
     })
+
+    const csv = parser.parse(json)
+    response.setHeader('content-Type', 'text/csv')
+    response.attachment('orders.csv')
+    response.send(csv)
+  }
+
+  async chart(): Promise<{ date: string; sum: string }[]> {
+    const apiData = await this.ordersRepository.query(`
+      SELECT to_date(cast(o.created_at as TEXT), '%Y-%m-%d') as date,
+             sum(oi.price * oi.quantity) as sum
+      FROM "orders" o
+      JOIN "order_item" oi ON o.id = oi.order_id
+      GROUP BY date;
+    `)
+
+    const chartData: { date: string; sum: string }[] = []
+    for (let index = 0; index < apiData.length; index++) {
+      chartData.push({
+        date: (apiData[index].date as Date).toISOString().split('T')[0],
+        sum: apiData[index].sum,
+      })
+    }
+    return chartData
   }
 }
